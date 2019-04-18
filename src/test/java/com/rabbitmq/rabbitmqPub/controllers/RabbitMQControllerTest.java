387b7e5ee460d819b2ac54cca3124974c911fce7
Sameer Neema <sameer.neema02@gmail.com>
2019-04-17 23:54:41 -0400

    unit test controller

--- src/test/java/com/rabbitmq/rabbitmqPub/controllers/RabbitMQControllerTest.java
+++ src/test/java/com/rabbitmq/rabbitmqPub/controllers/RabbitMQControllerTest.java
@@ -8,15 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class RabbitMQControllerTest {
@@ -45,24 +44,32 @@ class RabbitMQControllerTest {
         @DisplayName("When post to endpoint()")
         public class WhenPostToEndpoint {
 
-           @Test
-           @DisplayName("Then return ok status")
-           public void thenReturnOkStatus() throws Exception{
+            @Test
+            @DisplayName("Then return ok status")
+            public void thenReturnOkStatus() throws Exception {
 
 
-               doNothing().when(rabbitMQPubService).postToQueue(anyString());
+                doNothing().when(rabbitMQPubService).postToQueue(anyString());
 
-               mockMvc.perform(post("/queue/post/{message}", "hello")
-               .contentType(MediaType.APPLICATION_JSON_VALUE))
-                       .andExpect(status().isOk())
-                       .andReturn();
+                mockMvc.perform(post("/queue/post/{message}", "hello")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+            }
+
+            @Test
+            @DisplayName("Then verify post to queue is called")
+            public void thenVerifyPostToQueueIsCalled() {
 
 
-           }
+                doNothing().when(rabbitMQPubService).postToQueue(anyString());
+                rabbitMQController.publisher(anyString());
+                verify(rabbitMQPubService, times(1)).postToQueue(anyString());
+
+            }
 
         }
-
-
     }
 
 }
